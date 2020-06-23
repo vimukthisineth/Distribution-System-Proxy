296e3fa65d33c1eadd9e80e19aeaac87e8b820d0
Vimukthi Sineth <vimukthisineth@gmail.com>
2020-06-23 19:33:50 +0530

    Deliveries service added

--- src/main/java/com/sliit/research/blockchainbasedapplication/RestClients/BlockChainMicroserviceRestClient.java
+++ src/main/java/com/sliit/research/blockchainbasedapplication/RestClients/BlockChainMicroserviceRestClient.java
@@ -3,7 +3,9 @@ package com.sliit.research.blockchainbasedapplication.RestClients;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.sliit.research.blockchainbasedapplication.blockChain.Block;
 import com.sliit.research.blockchainbasedapplication.blockChain.BlockChain;
+import com.sliit.research.blockchainbasedapplication.dto.DeliveryRouteDto;
 import com.sliit.research.blockchainbasedapplication.model.BlockModel;
+import com.sliit.research.blockchainbasedapplication.utils.RoutePath;
 
 import java.io.IOException;
 import java.util.Arrays;
@@ -11,6 +13,7 @@ import java.util.List;
 
 public class BlockChainMicroserviceRestClient extends AbstractRestClient {
     private final String blockChainMicroserviceUrl = "http://localhost:8055/";
+    private final String deliveriesMicroserviceUrl = "http://localhost:8032/";
 
     public BlockModel newBlock(BlockModel blockModel) throws IOException {
         String response = this.post(blockChainMicroserviceUrl+"api/newBlock", mapper.writeValueAsString(blockModel));
@@ -25,5 +28,10 @@ public class BlockChainMicroserviceRestClient extends AbstractRestClient {
     public List<Block> getAllBlocksFromDb() throws IOException {
         String response = this.get(blockChainMicroserviceUrl+"api/getAllBlocksFromDb");
         return Arrays.asList(mapper.readValue(response, Block[].class));
+    }
+
+    public RoutePath calculateRoute(DeliveryRouteDto deliveryRoute) throws IOException {
+        String response = this.post(deliveriesMicroserviceUrl+"/calculateRoute", mapper.writeValueAsString(deliveryRoute));
+        return mapper.readValue(response, RoutePath.class);
     }
 }
